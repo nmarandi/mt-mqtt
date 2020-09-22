599d30350f6c41cb917a1ec1029b09fc4bbf17dd
nmarandi <nader.marandi69@gmail.com>
2020-09-22 15:34:24 +0330

    implement simple response for connect

--- src/client.rs
+++ src/client.rs
@@ -1,18 +1,22 @@
-use bytes::{Buf,BytesMut};
-use tokio::io::AsyncReadExt;
-use tokio::net::TcpStream;
-use std::io::Cursor;
+use crate::definitions::*;
 use crate::frame::*;
+use bytes::{Buf, BytesMut};
+use std::io::Cursor;
+use tokio::io::{AsyncReadExt, AsyncWriteExt, ReadHalf, WriteHalf};
+use tokio::net::TcpStream;
 
 pub struct Client {
-    stream: TcpStream,
+    read: ReadHalf<TcpStream>,
+    write: WriteHalf<TcpStream>,
     buffer: BytesMut,
 }
 
 impl Client {
     pub fn new(stream: TcpStream) -> Client {
+        let (rd, wr) = tokio::io::split(stream);
         Client {
-            stream,
+            read: rd,
+            write: wr,
             // Allocate the buffer with 4kb of capacity.
             buffer: BytesMut::with_capacity(4096),
         }
@@ -22,7 +26,7 @@ impl Client {
             // Attempt to parse a frame from the buffered data. If
             // enough data has been buffered, the frame is
             // returned.
-            if let Some(frame) = self.parse_frame()? {
+            if let Some(frame) = self.deserialize_frame()? {
                 return Ok(frame);
             }
 
@@ -31,7 +35,7 @@ impl Client {
             //
             // On success, the number of bytes is returned. `0`
             // indicates "end of stream".
-            if 0 == self.stream.read_buf(&mut self.buffer).await? {
+            if 0 == self.read.read_buf(&mut self.buffer).await? {
                 // The remote closed the connection. For this to be
                 // a clean shutdown, there should be no data in the
                 // read buffer. If there is, this means that the
@@ -44,12 +48,12 @@ impl Client {
             }
         }
     }
-    fn parse_frame(&mut self) -> Result<Option<Frame>,Error> {
+    fn deserialize_frame(&mut self) -> Result<Option<Frame>, Error> {
         // Create the `T: Buf` type.
         let mut buf = Cursor::new(&self.buffer[..]);
 
         // Check whether a full frame is available
-        match Frame::parse(&mut buf) {
+        match Frame::deserialize(&mut buf) {
             Ok(frame) => {
                 // Get the byte length of the frame
                 let len = buf.position() as usize;
@@ -61,16 +65,38 @@ impl Client {
                 Ok(Some(frame))
             }
             // Not enough data has been buffered
-            Err(Incomplete) => Ok(None),
+            Err(Error::Incomplete(_)) => Ok(None),
             // An error was encountered
             Err(e) => Err(e.into()),
         }
     }
+    pub async fn write_value(&mut self, src: &mut BytesMut) -> std::io::Result<()> {
+        self.write.write_buf(src).await?;
+        Ok(())
+    }
     pub async fn run(mut self) {
-        while let Ok(msg) = self.read_frame().await {
-            match msg {
-                Frame::Connect(header,connection_packet) => {
-                    
+        loop {
+            match self.read_frame().await {
+                Ok(msg) => {
+                    println!("connection_packet: {:?}", msg.control_packet);
+                    match msg.control_packet {
+                        ControlPacket::Connect(control_packet) => {
+                            self.write_value(
+                                &mut Frame::serialize(Frame::new(ControlPacketType::CONNACK))
+                                    .unwrap(),
+                            )
+                            .await
+                            .unwrap();
+                        }
+                        _ => break,
+                    }
+                }
+                // Not enough data has been buffered
+                Err(Error::Incomplete(_)) => println!("Not enough data has been buffered"),
+                // An error was encountered
+                Err(Error::Other(err)) => {
+                    println!("{}", err);
+                    break
                 },
             }
         }
